f674760e1a0227f86742b00366d369b6a3bddb85
kingsheril <34640986+kingsheril@users.noreply.github.com>
2018-05-16 22:39:23 +0530

    Create solonum.java

---
+++ solonum.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class solonum
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+int[] a=new int[n];
+for(int i=0;i<n;i++)
+a[i]=in.nextInt();
+int c;
+for(int i=0;i<n;i++)
+{
+c=0;
+for(int j=0;j<n;j++)
+{
+if((i!=j)&&(a[i]==a[i]))
+c++;
+}
+if(c==0)
+System.out.print(a[j]);
+}
+}
+}
